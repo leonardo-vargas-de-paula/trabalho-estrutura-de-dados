70377a9939ee61b6d42679b5115a83a48bfc16ca
Leonardo Vargas <leonardovargasdpaula@gmail.com>
2024-10-25 13:04:25 -0400

    feat: função que lê os pdfs da pasta e transforma em txt

--- code/Coversor.py
+++ code/Coversor.py
@@ -1,11 +1,21 @@
-import pymupdf #biblioteca para conversão
+import fitz  
+import re
+import os
 
-# Abrindo o arquivo PDF
-pdf_document = pymupdf.open("C:\\Users\\leonardo\\Desktop\\trabalho-estrutura-de-dados\\testePDF\pdfteste1.pdf")
-# Extraindo texto de cada p´agina
-with open("documento.txt", "w", encoding="utf-8") as txt_file:
-    for page_num in range(pdf_document.page_count):
-        page = pdf_document.load_page(page_num)
-        text = page.get_text()
-        txt_file.write(text + "\n")
-pdf_document.close()
+count = 1
+
+path = r'C:\\Users\\leonardo\\Desktop\\trabalho-estrutura-de-dados\\testePDF'
+
+for file in os.listdir(path):
+    if re.search('pdf', file):
+        print(file)
+        caminho_txt = "C:\\Users\\leonardo\\Desktop\\trabalho-estrutura-de-dados\\result\\doc" + str(count) + ".txt"
+        pdf_document = fitz.open("C:\\Users\\leonardo\\Desktop\\trabalho-estrutura-de-dados\\testePDF\\" + file)
+        with open(caminho_txt, "w", encoding="utf-8") as txt_file:
+            for page_num in range(pdf_document.page_count):
+                page = pdf_document.load_page(page_num)
+                text = page.get_text()
+                txt_file.write(text + "\n")
+            pdf_document.close()
+        count += 1
+        
